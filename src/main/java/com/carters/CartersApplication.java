73ae75bd0d168089ed58f0e68ef56bfe0349a680
Kostiantyn Bondar <kostiantyn_bondar@corside.com>
2015-10-11 22:17:55 +0300

    initial commit

--- src/main/java/com/carters/CartersApplication.java
+++ src/main/java/com/carters/CartersApplication.java
@@ -1,12 +1,12 @@
-package com.example;
+package com.carters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CartersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(CartersApplication.class, args);
     }
 }
